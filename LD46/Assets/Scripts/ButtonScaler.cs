f4d9feb30a038326d796bc92c2bd2043cd467edc
Konosprod <konosprod@free.fr>
2020-04-19 16:36:14 +0200

    Update main menu, add some music
    Signed-off-by: Konosprod <konosprod@free.fr>

---
+++ LD46/Assets/Scripts/ButtonScaler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonScaler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        this.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        this.transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+}
