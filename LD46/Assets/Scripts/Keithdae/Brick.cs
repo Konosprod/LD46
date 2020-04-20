1a2fe095ae155386fe0b0cf9488131899ea232de
Konosprod <konosprod@free.fr>
2020-04-20 18:27:42 +0200

    Add breaking sound
    Signed-off-by: Konosprod <konosprod@free.fr>

--- LD46/Assets/Scripts/Keithdae/Brick.cs
+++ LD46/Assets/Scripts/Keithdae/Brick.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
+    public AudioClip breakingSound;
+
     public enum BrickType
     {
         Normal = 0,
@@ -21,6 +23,7 @@ public class Brick : MonoBehaviour
 
     public void DestroyBrick()
     {
+        AudioManager.instance.PlaySfx(breakingSound);
         particles.time = 0;
         particles.Play();
         this.GetComponent<SpriteRenderer>().enabled = false;
