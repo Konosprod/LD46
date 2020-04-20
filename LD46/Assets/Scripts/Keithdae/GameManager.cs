e7e1327e269f8ee60596d229dd0beb8809caee0c
Konosprod <konosprod@free.fr>
2020-04-20 22:51:43 +0200

    Add new level indicator
    Signed-off-by: Konosprod <konosprod@free.fr>

--- LD46/Assets/Scripts/Keithdae/GameManager.cs
+++ LD46/Assets/Scripts/Keithdae/GameManager.cs
@@ -37,6 +37,8 @@ public class GameManager : MonoBehaviour
     private int level = 1;
     private float timer = 1337f;
 
+    public GameObject newLevelText;
+
     [Header("Panel Tuto")]
     public GameObject panelLevel1;
     public GameObject panelLevel3;
@@ -174,6 +176,13 @@ public class GameManager : MonoBehaviour
         SetupLevel();
     }
 
+    IEnumerator Co_NewLevel()
+    {
+        newLevelText.SetActive(true);
+        yield return new WaitForSeconds(3);
+        newLevelText.SetActive(false);
+    }
+
 
     private void StartLevel()
     {
@@ -213,6 +222,8 @@ public class GameManager : MonoBehaviour
         }
         else { SetupLevel(); }
 
+
+        StartCoroutine(Co_NewLevel());
     }
 
     public void GameOver()
